a7ce4d40753502300d50a9f061e6e684475ebc3a
dodypramana <edp.dody@gmail.com>
2021-05-09 22:58:15 +0700

    hapus modal yang tidak diperlukan

--- application/views/Components/main.php
+++ application/views/Components/main.php
@@ -41,80 +41,22 @@
 			  </div>
 			</div>
 
-			<!-- Modal pelaksana -->
-			<div class="modal fade" id="modal" role="dialog"  aria-hidden="true">
+			<!-- Modal preview -->
+
+			<div class="modal fade" id="modal_preview" tabindex="-1" role="dialog"  aria-hidden="true">
 			  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
 			    <div class="modal-content">
-			      <div class="modal-header">
-			        <h5 class="modal-title" id="title">Form pelaksana</h5>
-			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-			          <span aria-hidden="true">&times;</span>
-			        </button>
-			      </div>
 			      <div class="modal-body">
 			        <div class="row">
-			        	<div class="col-md-12">
-				        	<form id="form_pelaksana" method="post" enctype="multipart/form-data">
-					        	<div class="row" id="target">
-					        		
-					        	</div>
-					        	<div class="form-group" align="right">
-									<button class="btn btn-primary btn-sm" type="submit">Simpan</button>
-								</div>
-					        </form>
-					    </div>
+			        	<div class="col-md-12" id="target_preview">
+			        		
+			        	</div>
 			        </div>
 			      </div>
 			    </div>
 			  </div>
 			</div>
-
-			<!-- Modal kinerja -->
-			<div class="modal fade" id="modal_kinerja" role="dialog"  aria-hidden="true">
-			  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-			    <div class="modal-content">
-			      <div class="modal-header">
-			        <h5 class="modal-title" id="title">Form input kinerja kegiatan</h5>
-			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-			          <span aria-hidden="true">&times;</span>
-			        </button>
-			      </div>
-			      <div class="modal-body">
-			        <div class="row">
-			        	<div class="col-md-12">
-				        	<form id="form_kinerja_kegiatan" method="post" enctype="multipart/form-data">
-					        	<div class="row" id="target_kinerja">
-					        		
-					        	</div>
-					        	<div class="form-group" align="right">
-									<button class="btn btn-primary btn-sm" type="submit">Simpan</button>
-								</div>
-					        </form>
-					    </div>
-			        </div>
-			      </div>
-			    </div>
-			  </div>
-			</div>
-
-			<!-- Modal -->
-			<div class="modal fade" id="modal_" role="dialog"  aria-hidden="true">
-			  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-			    <div class="modal-content">
-			      <div class="modal-header">
-			        <h5 class="modal-title" id="title_modal"></h5>
-			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-			          <span aria-hidden="true">&times;</span>
-			        </button>
-			      </div>
-			      <div class="modal-body">
-			        <div class="row" id="target_detail">
-			        	
-			        </div>
-			      </div>
-			    </div>
-			  </div>
-			</div>
+			
 			<!-- footer -->
 			<?php $this->load->view('Components/footer')?>
 			<!-- end footer -->
